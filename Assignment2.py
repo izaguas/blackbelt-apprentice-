cd15e91261adf8f91948ee0338f045772654662e
izaguas <39487224+izaguas@users.noreply.github.com>
2018-05-21 20:17:39 +0800

    Add files via upload

---
+++ Assignment2.py
@@ -0,0 +1,24 @@
+#Libraries and Functions always come in handy to developers by allowing reusability of existing code.
+# There are certain well known inherent libraries that you have access to after installing python.
+# By using these libraries and functions in them, write a program (in Python 3) to guess a randomly generated number between 1 and 10.
+#For Example:
+
+#Guess the number: 4
+#Wrong, try again!
+
+#Guess the number: 8
+#Correct!
+
+#Hint: Figure out which library the “randint” function belongs to.
+import random
+
+number = random.randint(0, 9)
+#print (number)
+print('Guess the number from 0 to 9')
+guess = input()
+guess = int(guess)
+if guess == number:
+    print("Correct!")
+else:
+    print("Wrong, try again!")
+
